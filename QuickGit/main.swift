cd9ef0896ec630e009adf481c6aba1beaa948c34
Steven Sherry <steven.r.sherry@gmail.com>
2018-02-17 01:12:01 -0600

    Created funcationality to add all files, commit, and push to remote repo

--- QuickGit/main.swift
+++ QuickGit/main.swift
@@ -4,36 +4,8 @@ import Foundation
 let curentDirectory = FileManager.default.currentDirectoryPath
 
 let arguments = CommandLine.arguments
-let language: String?
+var language: String?
 var remoteRepo: String?
-
-switch arguments.count {
-case 2:
-  language = arguments[1]
-case 3:
-  language = arguments[1]
-  remoteRepo = arguments[2]
-default:
-  language = nil
-  remoteRepo = nil
-}
-
-var gitignoreText: String? = nil
-
-if let language = language {
-  switch language.lowercased() {
-  case "swift", "objective-c", "objc", "xcode":
-    gitignoreText = GitIgnoreText.swiftAndObjC.rawValue
-  case "java", "kotlin", "android":
-    gitignoreText = GitIgnoreText.android.rawValue
-  default:
-    break
-  }
-} else {
-  gitignoreText = GitIgnoreText.swiftAndObjC.rawValue
-}
-
-
 
 @discardableResult
 func shell(_ args: String...) -> Int32 {
@@ -45,19 +17,62 @@ func shell(_ args: String...) -> Int32 {
   return task.terminationStatus
 }
 
-let gitignoreURL = URL(fileURLWithPath: "\(curentDirectory)/.gitignore")
-
-try! gitignoreText?.write(to: gitignoreURL, atomically: true, encoding: .ascii)
-
-shell("git", "init")
-shell("git", "add", "-A")
-shell("git", "commit", "-m", "\"Initial commit with .gitignore\"")
-
-guard let remoteRepo = remoteRepo else {
-  exit(0)
+func initAndCommit(args: String?...) {
+  var gitignoreText: String? = nil
+  language = args[0]
+  remoteRepo = args[1]
+  if let language = language {
+    switch language.lowercased() {
+    case "swift", "objective-c", "objc", "xcode":
+      gitignoreText = GitIgnoreText.swiftAndObjC.rawValue
+    case "java", "kotlin", "android":
+      gitignoreText = GitIgnoreText.android.rawValue
+    default:
+      break
+    }
+  } else {
+    gitignoreText = GitIgnoreText.swiftAndObjC.rawValue
+  }
+  
+  let gitignoreURL = URL(fileURLWithPath: "\(curentDirectory)/.gitignore")
+  
+  try! gitignoreText?.write(to: gitignoreURL, atomically: true, encoding: .ascii)
+  
+  shell("git", "init")
+  shell("git", "add", "-A")
+  shell("git", "commit", "-m", "\"Initial commit with .gitignore\"")
+  
+  guard let remoteRepo = remoteRepo else {
+    exit(0)
+  }
+  
+  shell("git", "remote", "add", "origin", remoteRepo)
+  shell("git", "push", "origin", "master")
 }
 
-shell("git", "remote", "add", "origin", remoteRepo)
-shell("git", "push", "origin", "master")
+func quickCommit(commitMessage: String) {
+  shell("git", "add", "-A")
+  shell("git", "commit", "-m", "\(commitMessage)")
+  shell("git", "push", "origin", "master")
+}
+
+switch arguments.count {
+case 2:
+  initAndCommit(args: arguments[1])
+case 3:
+  if arguments[1] == "commit" {
+    quickCommit(commitMessage: arguments[2])
+  } else {
+    initAndCommit(args: arguments[1], arguments[2])
+  }
+default:
+  break
+}
+
+
+
+
+
+
 
 
